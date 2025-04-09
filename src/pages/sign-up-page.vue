2ba239d3b4328257615b4ef2734d8335ef26c4d3
Crinax <agreedeth@gmail.com>
2025-04-10 00:30:56 +0300

    feat(sign-up/page): draft a sign up window

--- src/pages/sign-up-page.vue
+++ src/pages/sign-up-page.vue
@@ -1,7 +1,32 @@
 <script setup lang="ts">
 import SignUp from '@features/sign-up/ui/sign-up.vue';
+import { NButton, NCard, NFlex } from 'naive-ui';
 </script>
 
 <template>
-  <SignUp />
+  <n-flex justify="center" align="center" class="sign-up__wrapper">
+    <n-card class="sign-up__card" title="Sign Up">
+      <sign-up />
+
+      <template #action>
+        <n-flex justify="end">
+          <n-button>Sign Up</n-button>
+        </n-flex>
+      </template>
+    </n-card>
+  </n-flex>
 </template>
+
+<style lang="scss" scoped>
+.sign-up {
+  &__card {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 600px;
+  }
+
+  &__wrapper {
+    height: 100%;
+  }
+}
+</style>

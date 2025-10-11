2e4dc2dc24192e33cc34889156e9bb8df49e9811
Crinax <agreedeth@gmail.com>
2025-10-11 23:42:52 +0300

    fix: remove tailwind classes from h1 in about page

--- src/pages/about/ui/About.tsx
+++ src/pages/about/ui/About.tsx
@@ -6,9 +6,7 @@ import type { FC } from 'react';
 export const AboutPage: FC = () => {
   return (
     <div>
-      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
-        about page
-      </h1>
+      <h1>about page</h1>
       <Button component={Link} to="/">
         Home
       </Button>

11b740f5208e8fcdd9c7e09cf5b430542d8aa33e
Crinax <agreedeth@gmail.com>
2025-11-04 20:22:09 +0300

    refactor(app): remove animations

--- src/pages/channel/ui/chat-sidebar/index.tsx
+++ src/pages/channel/ui/chat-sidebar/index.tsx
@@ -1,7 +1,5 @@
-import { Button, Group, ScrollArea, Stack, Transition } from '@mantine/core';
+import { Button, Group, ScrollArea, Stack } from '@mantine/core';
 import { IconPlus } from '@tabler/icons-react';
-
-import { useIsMounted } from '../../../../shared/react';
 
 import { ChatList } from './../chat-list';
 import styles from './styles.module.scss';
@@ -9,34 +7,23 @@ import styles from './styles.module.scss';
 import type { FC } from 'react';
 import type { ChatSidebarProps } from './types';
 
-export const ChatSidebar: FC<ChatSidebarProps> = ({ children, ...rest }) => {
-  const isMounted = useIsMounted();
+export const ChatSidebar: FC<ChatSidebarProps> = ({ children, ...rest }) => (
+  <Group className={styles['app-chat-sidebar__wrapper']}>
+    <ScrollArea className={styles['app-chat-sidebar']}>
+      <Stack gap="xs">
+        <ChatList {...rest} />
 
-  return (
-    <Transition mounted={isMounted} transition="slide-right">
-      {(transitionStyles) => (
-        <Group
-          className={styles['app-chat-sidebar__wrapper']}
-          style={transitionStyles}
+        <Button
+          fullWidth
+          variant="light"
+          color="cyan"
+          leftSection={<IconPlus strokeLinecap="round" />}
         >
-          <ScrollArea className={styles['app-chat-sidebar']}>
-            <Stack gap="xs">
-              <ChatList {...rest} />
+          Новый чат
+        </Button>
+      </Stack>
+    </ScrollArea>
 
-              <Button
-                fullWidth
-                variant="light"
-                color="cyan"
-                leftSection={<IconPlus strokeLinecap="round" />}
-              >
-                Новый чат
-              </Button>
-            </Stack>
-          </ScrollArea>
-
-          {children}
-        </Group>
-      )}
-    </Transition>
-  );
-};
+    {children}
+  </Group>
+);
--- src/pages/chat/ui/chat-page/index.tsx
+++ src/pages/chat/ui/chat-page/index.tsx
@@ -1,22 +1,12 @@
-import { Flex, Transition } from '@mantine/core';
-
-import { useIsMounted } from '../../../../shared/react';
+import { Flex } from '@mantine/core';
 
 import styles from './styles.module.scss';
 
 import type { FC } from 'react';
 import type { ChatPageProps } from './types';
 
-export const ChatPage: FC<ChatPageProps> = ({ chatId, channelId }) => {
-  const isMounted = useIsMounted();
-
-  return (
-    <Transition mounted={isMounted} transition="scale">
-      {(transitionStyles) => (
-        <Flex className={styles['app-chat']} style={transitionStyles}>
-          You now in chat: {chatId} in channel: {channelId}
-        </Flex>
-      )}
-    </Transition>
-  );
-};
+export const ChatPage: FC<ChatPageProps> = ({ chatId, channelId }) => (
+  <Flex className={styles['app-chat']}>
+    You now in chat: {chatId} in channel: {channelId}
+  </Flex>
+);

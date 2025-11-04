000e10a06cad3cdea8a6630e1efa848937f637ab
Evgeniy Gritsenko <evgritsenko@inbox.ru>
2025-11-05 02:49:02 +0600

    feat(pages/chat): add message input field

--- src/pages/chat/ui/chat-page/index.tsx
+++ src/pages/chat/ui/chat-page/index.tsx
@@ -1,14 +1,39 @@
-import { Flex } from '@mantine/core';
+import { Box, Flex, Stack, TextInput } from '@mantine/core';
+import { useState, type FC } from 'react';
 
 import { MessageList } from '../message-list';
 
 import styles from './styles.module.scss';
 
-import type { FC } from 'react';
 import type { ChatPageProps } from './types';
 
-export const ChatPage: FC<ChatPageProps> = ({ chatId, channelId }) => (
-  <Flex className={styles['app-chat']}>
-    <MessageList channelId={channelId} chatId={chatId} />
-  </Flex>
-);
+export const ChatPage: FC<ChatPageProps> = ({ chatId, channelId }) => {
+  const [value, setValue] = useState('');
+
+  const sendMessage = (e: React.FormEvent): void => {
+    e.preventDefault();
+    // TODO: отправить сообщение
+  };
+
+  // TODO: добавить нормальное отображение переносов строк в поле ввода
+  return (
+    <Flex className={styles['app-chat__wrapper']}>
+      <Stack className={styles['app-chat']}>
+        <Box className={styles['app-chat__message-list']}>
+          <MessageList channelId={channelId} chatId={chatId} />
+        </Box>
+        <form onSubmit={sendMessage}>
+          <TextInput
+            classNames={{
+              input: styles['app-chat__message-input'],
+            }}
+            value={value}
+            onChange={(e) => setValue(e.currentTarget.value)}
+            placeholder="Сообщение..."
+            radius="md"
+          />
+        </form>
+      </Stack>
+    </Flex>
+  );
+};

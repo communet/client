f21c2b99f16e4ecdb8fae2033b19b59bccba2890
Evgeniy Gritsenko <evgritsenko@inbox.ru>
2025-11-24 22:44:53 +0600

    Feat/chat message input (#24)
    * feat(routing): add chat routing and animations
    
    * refactor(app): remove animations
    
    * feat(pages/chat): add message list
    
    * feat(pages/chat): add message input field
    
    * fix(pages/chat): use useCallback for sendMessage
    
    ---------
    
    Co-authored-by: Crinax <agreedeth@gmail.com>

--- src/pages/chat/ui/chat-page/index.tsx
+++ src/pages/chat/ui/chat-page/index.tsx
@@ -1,14 +1,43 @@
-import { Flex } from '@mantine/core';
+import { Box, Flex, Stack, TextInput } from '@mantine/core';
+import { useCallback, useState, type FC } from 'react';
 
 import { MessageList } from '../message-list';
 
 import styles from './styles.module.scss';
 
-import type { FC } from 'react';
 import type { ChatPageProps } from './types';
 
-export const ChatPage: FC<ChatPageProps> = ({ chatId, channelId }) => (
-  <Flex className={styles['app-chat']}>
-    <MessageList channelId={channelId} chatId={chatId} />
-  </Flex>
-);
+export const ChatPage: FC<ChatPageProps> = ({ chatId, channelId }) => {
+  const [value, setValue] = useState('');
+
+  const sendMessage = useCallback(
+    (e: React.FormEvent): void => {
+      e.preventDefault();
+      console.log(value);
+      // TODO: отправить сообщение
+    },
+    [value],
+  );
+
+  // TODO: добавить нормальное отображение переносов строк в поле ввода
+  return (
+    <Flex className={styles['app-chat__wrapper']}>
+      <Stack className={styles['app-chat']}>
+        <Box className={styles['app-chat__message-list']}>
+          <MessageList channelId={channelId} chatId={chatId} />
+        </Box>
+        <form onSubmit={sendMessage}>
+          <TextInput
+            classNames={{
+              input: styles['app-chat__message-input'],
+            }}
+            value={value}
+            onChange={(e) => setValue(e.currentTarget.value)}
+            placeholder="Сообщение..."
+            radius="md"
+          />
+        </form>
+      </Stack>
+    </Flex>
+  );
+};
